afbd55ed1b8596d65f70de4465729177819ebc82
nateapathy <nate.apathy@gmail.com>
2022-01-19 08:56:41 -0500

    initial commit + words

---
+++ setup/get_word_lists.R
@@ -0,0 +1,8 @@
+#library(googlesheets4)
+library(tidyverse)
+mystery_words <- googlesheets4::read_sheet(ss="https://docs.google.com/spreadsheets/d/1-M0RIVVZqbeh0mZacdAsJyBrLuEmhKUhNaVAI-7pr2Y/edit#gid=0",col_names = F) %>% pull()
+
+guessable_words <- googlesheets4::read_sheet(ss="https://docs.google.com/spreadsheets/d/1KR5lsyI60J1Ek6YgJRU2hKsk4iAOWvlPLUWjAZ6m8sg/edit#gid=0",col_names = F) %>% pull()
+# https://docs.google.com/spreadsheets/d/1KR5lsyI60J1Ek6YgJRU2hKsk4iAOWvlPLUWjAZ6m8sg/edit#gid=0
+
+save(mystery_words,guessable_words,file = "words.Rdata")
