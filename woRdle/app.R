b24bfa139f95022febadbbfe1fc25a5bff783fec
NateApathy <nate.apathy@gmail.com>
2022-01-23 00:48:14 -0500

    going backwards

--- woRdle/app.R
+++ woRdle/app.R
@@ -9,14 +9,15 @@ set.seed(as.numeric(Sys.Date()))
 # sets a new seed each day, so the word changes each day
 word <- sample(mystery_words,1)
 ltrs <- strsplit(word,"")[[1]]
-gdf <- tibble(g=word,
+gdf <- tibble(g="AAAAA",
               lno=c(1:5),
               ltr=ltrs,
-              cor=TRUE)
+              cor=TRUE,
+              orig=TRUE)
 
 # Define UI for application
 ui <- fluidPage(align="center",
-  textInput("guess_in","Your Guess:"),
+  textInput("guess_in","Guess:"),
   actionButton("guess","Guess!"),
   textOutput("guess_out"),
   # dynamically add a row of boxes each time they submit a guess
@@ -25,13 +26,7 @@ ui <- fluidPage(align="center",
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  all_guesses <- eventReactive(input$guess==1,{
-    as.character()
-  })
-  all_guesses <- eventReactive(input$guess>1,{
-      append(all_guesses,toupper(input$guess_in))
-  })
-  
+
   guesscheck <- eventReactive(input$guess,{
     # when they hit guess, store the guess
     g <- toupper(input$guess_in)
@@ -41,36 +36,28 @@ server <- function(input, output) {
     )
     g
   })
-  
+
   output$guess_out <- renderText({
     guesscheck()
   })
   
   guessdata <- eventReactive(input$guess,{
-    
-    gdf %>% bind_rows(
-      tibble(g=toupper(input$guess_in),
-             lno=c(1:5)) %>%
-        mutate(ltr=strsplit(g,"")[[1]],
-               cor=ltr==ltrs,
-               inword=case_when(ltr %in% ltrs~TRUE,
-                                TRUE~FALSE),
-               acc=case_when(cor==T~2,
-                             cor==F&inword==T~1,
-                             TRUE~0))
-    ) -> gdf
-    
-    gdf
+    g <- str_split(toupper(input$guess_in),"")[[1]]
+    #right <- input$guess_in==word # check if it's right; technically don't even need this
+    tibble(l=g,
+           ltrs=g %in% ltrs, # check if letters are in word
+           spots=g==ltrs) %>% # check if letters are in the right place
+      mutate(color=case_when(spots==T~"green",
+                             spots==F&ltrs==T~"yellow",
+                             spots==F&ltrs==F~"grey"),
+             lno=c(1:5)) -> gdat
+    gdat
     # only update data if it's a new legit guess
   })
   
   output$resplot <- renderPlot({
     guessdata() %>%
-      group_by(g) %>%
-      filter(g!=word) %>%
-      mutate(gno=as.numeric(factor(g)),
-             gord=factor(gno,levels = rev(unique(gno)))) %>%
-      ggplot(aes(x=lno,y=gord,fill=factor(acc),label=ltr)) +
+      ggplot(aes(x=lno,y=1,fill=color,label=l)) +
       geom_tile(color="black",size=2) +
       geom_text(size=10) +
       scale_fill_manual(values=c("grey","yellow","green")) +
--- wordle_scratch.R
+++ wordle_scratch.R
@@ -1,6 +1,46 @@
 library(tidyverse)
 load("words.Rdata")
 
+word <- "ABATE"
+guess <- "TABLE"
+word==guess # check if it's right; technically don't even need this
+cbind(str_split(guess,"")[[1]] %in% str_split(word,"")[[1]], # check if letters are in word
+str_split(guess,"")[[1]]==str_split(word,"")[[1]]) # check if letters are in the right place
+
+g <- str_split(toupper("being"),"")[[1]]
+#right <- input$guess_in==word # check if it's right; technically don't even need this
+tibble(l=g,
+       ltrs=g %in% ltrs, # check if letters are in word
+       spots=(g==ltrs)) %>% # check if letters are in the right place
+  mutate(color=case_when(spots==T~"green",
+                         spots==F&ltrs==T~"yellow",
+                         spots==F&ltrs==F~"grey"),
+         lno=c(1:5)) -> gdat
+gdat
+
+gdf %>% bind_rows(
+  tibble(g=toupper("being"),
+         lno=c(1:5)) %>%
+    mutate(ltr=strsplit(g,"")[[1]],
+           cor=ltr==ltrs,
+           inword=case_when(ltr %in% ltrs~TRUE,
+                            TRUE~FALSE),
+           acc=case_when(cor==T~2,
+                         cor==F&inword==T~1,
+                         TRUE~0))) %>%
+  filter(g!="AAAAA") %>%
+  group_by(g) %>%
+  mutate(gno=as.numeric(factor(g)),
+         gord=factor(gno,levels = rev(unique(gno)))) %>%
+  #filter(orig!=TRUE) %>%
+  ggplot(aes(x=lno,y=gord,fill=factor(acc),label=ltr)) +
+  geom_tile(color="black",size=2) +
+  geom_text(size=10) +
+  scale_fill_manual(values=c("grey","yellow","green")) +
+  theme_void() +
+  theme(legend.position = "none")
+
+
 word <- "BYLAW"
 ltrs <- strsplit(word,"")[[1]]
 word_df <- tibble(gno=0,
